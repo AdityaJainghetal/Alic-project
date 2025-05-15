c133751971417f02569b9be976c808ac1b6cdc26
Aditya Jain <adityajainghetal@gmail.com>
2025-05-15 21:29:33 +0530

    Task

--- Server/Utils/imageKit.js
+++ Server/Utils/imageKit.js
@@ -3,9 +3,9 @@ const ImageKit = require('imagekit');
 // Initialize ImageKit with your credentials
 const imagekit = new ImageKit({
 
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,   // Public API Key from your ImageKit dashboard
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT, // Your URL endpoint from the dashboard
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // Private API Key from your ImageKit dashboard
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY, 
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT, 
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 module.exports = imagekit;
